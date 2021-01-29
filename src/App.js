bf60bdcb6d4eb84f8b73bc60cdb63565c1257d40
mephisto2120 <mephisto2120@gmail.com>
2021-01-29 21:05:07 +0100

    Consuming REST api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
 
 function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/mephisto2120/repos`;
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+  }, [setAppState]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+        <div className='container'>
+          <h1>My Repositories</h1>
+        </div>
+        <div className='repo-container'>
+          <ListLoading isLoading={appState.loading} repos={appState.repos} />
+        </div>
+        <footer>
+          <div className='footer'>
+            Built{' '}
+            <span role='img' aria-label='love'>
+            💚
+          </span>{' '}
+            with by mephisto2120
+          </div>
+        </footer>
+      </div>
   );
 }
 
