484d495f301d42bfae313f0c3b17ad3cd4373bbb
mephisto2120 <mephisto2120@gmail.com>
2021-01-29 21:11:46 +0100

    Fixing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders My Repositories header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My Repositories/i);
   expect(linkElement).toBeInTheDocument();
 });
