113e5fc89e26a8ae25891618aa1227eaa9203446
mephisto2120 <mephisto2120@gmail.com>
2021-02-09 15:40:08 +0100

    Reorganizing structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,43 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Person} from './model/interfaces'
-import {SuperApi} from "./model/SuperApi";
+import {PersonService} from "./service/PersonService";
 
 
 function App() {
-  const [person, setPerson] = useState<Person[]>([]);
+    const [person, setPerson] = useState<Person[]>([]);
 
-  useEffect(() => {
-    SuperApi.getApi()
-        .then((person: Person[]) => {
-          setPerson(person);
-        })
-  }, []);
+    useEffect(() => {
+        PersonService.getApi()
+            .then((person: Person[]) => {
+                setPerson(person);
+            })
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {person.map(item => item.firstName)
-            /*{person[0].firstName} {person[0].lastName} */}
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    {person.map(item => item.firstName + ' ' + item.lastName)}
+                </p>
+                <p>
+                    {person[0].firstName} {person[0].lastName}
+                </p>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/service/PersonService.ts
+++ src/service/PersonService.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-export class SuperApi {
+export class PersonService {
     static getApi = async () => {
         try {
-            // /person/get?customerId=987
-            //http://localhost:8080/person?
             const result = await axios.get('http://localhost:8080/person/get?customerId=987');
             return result.data;
         } catch (e) {
