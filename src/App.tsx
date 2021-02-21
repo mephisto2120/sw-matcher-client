87563b373ebc29d584693f53aaa8035b7d35a073
mephisto2120 <mephisto2120@gmail.com>
2021-02-21 21:58:41 +0100

    Copying working solution

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import {Person} from './model/interfaces'
 import {PersonService} from "./service/PersonService";
 import {PersonList} from "./view/Person";
 
+import { Provider } from 'react-redux';
+import RepositoriesList from "./components/RepositoriesList";
+import {store} from "./state";
+
 function App() {
 
     const [person, setPerson] = useState<Person[]>([]);
@@ -17,29 +21,35 @@ function App() {
     }, []);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <div className='repo-container'>
-                    <PersonList persons={person}></PersonList>
-                </div>
+        <Provider store={store}>
+            <div>
+                <h1>Search For a Package</h1>
+                <RepositoriesList />
+            </div>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <div className='repo-container'>
+                        <PersonList persons={person}></PersonList>
+                    </div>
 
-                <p>
-                    {person.map(item => item.firstName + ' ' + item.lastName)}
-                </p>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+                    <p>
+                        {person.map(item => item.firstName + ' ' + item.lastName)}
+                    </p>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        </Provider>
     );
 }
 
