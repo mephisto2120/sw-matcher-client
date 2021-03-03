4525f4074e08f9fd119b0718903a2daa96792dcf
mephisto2120 <mephisto2120@gmail.com>
2021-03-03 11:31:08 +0100

    Testing person view

--- src/components/__tests__/PersonView.test.tsx
+++ src/components/__tests__/PersonView.test.tsx
@@ -4,14 +4,18 @@ import PersonView from 'components/PersonView';
 import Root from 'Root';
 
 it('should contain fields for searching', () => {
-  // const component = mount(
-  //   <Root>
-  //     <PersonView/>
-  //   </Root>
-  // );
-  //
-  // const firstNameDivText = component.find({id: "firstName"}).text();
-  //
-  // expect(firstNameDivText).toEqual('First name');
-  // component.unmount();
+  const component = mount(
+    <Root>
+      <PersonView/>
+    </Root>
+  );
+
+  const customerIdDivText = component.find({id: "customerId"}).text();
+  const firstNameDivText = component.find({id: "firstName"}).text();
+  const lastNameDivText = component.find({id: "lastName"}).text();
+
+  expect(customerIdDivText).toEqual('Customer id');
+  expect(firstNameDivText).toEqual('First name');
+  expect(lastNameDivText).toEqual('Last name');
+  component.unmount();
 });
